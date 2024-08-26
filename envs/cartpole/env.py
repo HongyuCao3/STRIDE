b179ccc8b04f31bae1837d3e70641bd5956b35d8
HongyuCao <2791897487@qq.com>
2024-08-26 16:12:12 +0800

    add basic
    logic about agent name and choice, need more description and cartpole env involved

--- envs/cartpole/env.py
+++ envs/cartpole/env.py
@@ -1,6 +1,6 @@
 import sys, os
 import gym
-from typing import List, Optional,
+from typing import List, Optional
 from pydantic import BaseModel
 from envs.env_helper import BaseEnv
 
@@ -21,6 +21,42 @@ class State(BaseModel):
             print("Illegal action {}.".format(action))
             return False
         
-
+class CartPole(BaseEnv):
+    def __init__(self, env_param):
+        self.name = "cart_pole"
+        self.required_agents = ["controller"]
+        self.env_param = env_param
+        
+        # TODO: 等待设置具体参数
+        
+        self.proposal = {0, 1} 
+        # agents can make choice in the set
+        self.reset()
+        
+    def get_description(self, agent_role=None):
+        assert agent_role == "controller"
+        description = "Now you are going to play in a cart pole problem, with "
+            # TODO: 设计描述
+        return description
+    
+    def reset(self):
+        # TODO: 根据cartpole重置状态
+        self.state = State(time_step=1, cur_agent="controller", actions=self.proposal)
+        self.is_done = False
+    
+    def step(self, action):
+        # TODO: 根据action更新状态
+        pass
+    
+     
 if __name__ == "__main__":
-    pass
+    env = CartPole()
+    state = env.state
+    print(state.textual_descript)
+    
+    while not env.is_done:
+        action = input("What is your decision\n")
+        if env.state.actions == {0,1}:
+            # TODO: 获取当前pole的状态
+            pass
+        state = env.step(action)
